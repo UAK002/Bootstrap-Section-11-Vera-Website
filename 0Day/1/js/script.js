db41c278db2229188865fdaf67e2ea078a1fcd24
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-12-17 07:13:24 +0530

    R01

--- 0Day/1/js/script.js
+++ 0Day/1/js/script.js
@@ -11,4 +11,23 @@ if (checkReplace !== null) {
   });
 }
 
-console.log(checkReplace);
+// User Scroll for navbar
+const userScroll = () => {
+  const navbar = document.querySelector('.navbar');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add('bg-dark');
+      navbar.classList.add('border-bottom');
+      navbar.classList.add('border-secondary');
+      navbar.classList.add('navbar-sticky');
+    } else {
+      navbar.classList.remove('bg-dark');
+      navbar.classList.remove('border-bottom');
+      navbar.classList.remove('border-secondary');
+      navbar.classList.remove('navbar-sticky');
+    }
+  });
+};
+
+document.addEventListener('DOMContentLoaded', userScroll);
